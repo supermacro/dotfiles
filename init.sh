5382c66667a4f831e29146b3b708c22b39a33a2a
Giorgio Delgado <hi@gdelgado.ca>
2022-10-07 13:20:04 -0400

    Add diff-so-fancy

--- init.sh
+++ init.sh
@@ -162,6 +162,33 @@ else
 fi
 
 
+if ! command -v diff-so-fancy &> /dev/null
+then
+    info "installing diff-so-fancy"
+    brew install diff-so-fancy 
+
+    info "updating git settings to use diff-so-fancy"
+    git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+    git config --global interactive.diffFilter "diff-so-fancy --patch"
+
+    git config --global color.ui true
+
+    git config --global color.diff-highlight.oldNormal    "red bold"
+    git config --global color.diff-highlight.oldHighlight "red bold 52"
+    git config --global color.diff-highlight.newNormal    "green bold"
+    git config --global color.diff-highlight.newHighlight "green bold 22"
+
+    git config --global color.diff.meta       "11"
+    git config --global color.diff.frag       "magenta bold"
+    git config --global color.diff.func       "146 bold"
+    git config --global color.diff.commit     "yellow bold"
+    git config --global color.diff.old        "red bold"
+    git config --global color.diff.new        "green bold"
+    git config --global color.diff.whitespace "red reverse"
+else 
+    warn "diff-so-fancy already installed, skipping diff-so-fancy install"
+fi
+
 if ! command -v kitty &> /dev/null
 then
     info "kitty not installed, installing kitty now"
