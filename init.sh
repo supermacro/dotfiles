0bf0d4d07b85356544212d00563376e532a4fe80
Giorgio Delgado <hi@gdelgado.ca>
2022-10-07 13:42:33 -0400

    Set up docker

--- init.sh
+++ init.sh
@@ -16,9 +16,10 @@ warn () {
 }
 
 
-# update git
+# update apt package index
+sudo apt update 
+sudo apt upgrade -y
 
-sudo apt update && sudo apt upgrade -y
 
 # update git
 sudo add-apt-repository ppa:git-core/ppa -y
@@ -197,6 +198,30 @@ then
     ln -s ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 else
     warn "kitty already installed, skipping install"
+fi
+
+if ! command -v docker &> /dev/null
+then
+    # https://docs.docker.com/engine/install/ubuntu
+    info "installing docker"
+    sudo apt install \
+        ca-certificates \
+        lsb-release
+
+    info "adding docker's GPG key"
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    info "setting up docker repository"
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    info "installing docker engine"
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+else
+    info "docker already installed, skipping docker installation"
 fi
 
 if ! command -v gcloud &> /dev/null
